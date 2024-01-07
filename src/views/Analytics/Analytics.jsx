f35a48aa9cc73fa2c5f4a1750cd8bef1406ddb4d
Mohamed7599 <mohamedmagdy7599@gmail.com>
2024-01-07 06:56:17 +0200

    reversing rows in small screens so the map apperas first

--- src/views/Analytics/Analytics.jsx
+++ src/views/Analytics/Analytics.jsx
@@ -9,8 +9,8 @@ const Analytics = () => {
         <div className='pr-5 py-8 flex flex-col gap-5'>
             <div className='grid grid-cols-12 gap-3'>
                 <div className="col-span-12">
-                    <Card isTitleDefaultColor={true} titleSize={'text-lg'} className={'relative grid grid-cols-12 '} >
-                        <div className='col-span-6 pt-10'>
+                    <Card isTitleDefaultColor={true} titleSize={'text-lg'} className={'relative lg:grid lg:grid-cols-12 flex flex-col-reverse'} >
+                        <div className='col-span-12 lg:col-span-6 pt-10'>
                             <div className=' absolute top-[10%] translate-y-[-90%] left-7 flex items-center gap-5'>
                                 <span className='bg-gradient-success p-4 rounded-xl'>
                                     <TbWorld size={30} className='text-white rounded-lg ' />
@@ -23,7 +23,7 @@ const Analytics = () => {
                                 <CustomTable data={salesByCountryTable} columns={salesByCountryCols} />
                             </div>
                         </div>
-                        <div className='col-span-6 h-96 '>
+                        <div className='col-span-12 lg:col-span-6 h-96 pt-10 lg:pt-0 '>
                             <Maps />
                         </div>
                     </Card>
